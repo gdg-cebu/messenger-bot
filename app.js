b7e27f99c4e78598a3c14534c537ee396e66fd2a
Arnelle Balane <arnellebalane@gmail.com>
2016-09-09 14:15:55 +0800

    get route for webhook verification

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const morgan = require('morgan');
+const logger = require('./config/logger');
+const config = require('./config/config');
 
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyparser.json());
+
+
+app.get('/webhook', (req, res) => {
+    if (req.query['hub.mode'] === 'subscribe'
+    && req.query['hub.verify_token'] === config.get('FB_VERIFY_TOKEN')) {
+        logger.info('Webhook validation successful.');
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        logger.error('Webhook validation failed.');
+        res.sendStatus(403);
+    }
+});
 
 
 module.exports = app;
